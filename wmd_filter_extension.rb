68d41f3331b0105c8761a3c44c7d6877b3361469
Jim Gay <jim@saturnflyer.com>
2009-07-16 15:52:44 -0400

    radiant 0.8.0

--- wmd_filter_extension.rb
+++ wmd_filter_extension.rb
@@ -1,19 +1,17 @@
-require_dependency 'application'
+require_dependency 'application_controller'
 
 class WmdFilterExtension < Radiant::Extension
   version "1.1"
-  description "WMD Markdown visual editor (<a href='http://wmd-editor.com'>http://wmd-editor.com</a>)"
+  description "WMD Markdown visual editor (http://wmd-editor.com)"
   url "http://github.com/saturnflyer/radiant-wmd_filter-extension"
   
   def activate
-    if ActiveRecord::Base.connection.tables.include?('config')
-      Radiant::Config['wmd.output'] = "Markdown" unless Radiant::Config['wmd.output']
-      Radiant::Config['wmd.lineLength'] = "60" unless Radiant::Config['wmd.lineLength']
-      Radiant::Config['wmd.buttons'] = "bold italic | link blockquote code image | ol ul heading hr" unless Radiant::Config['wmd.buttons']
-      Radiant::Config['wmd.autostart'] = "false" unless Radiant::Config['wmd.autostart']
-    end
-    admin.page.edit.add :parts_bottom, 'filter_preview', :before => 'edit_layout_and_type'
-    admin.snippet.edit.add :form_bottom, 'admin/pages/filter_preview', :after => 'edit_buttons'
+    Radiant::Config['wmd.output'] = "Markdown" unless Radiant::Config['wmd.output']
+    Radiant::Config['wmd.lineLength'] = "60" unless Radiant::Config['wmd.lineLength']
+    Radiant::Config['wmd.buttons'] = "bold italic | link blockquote code image | ol ul heading hr" unless Radiant::Config['wmd.buttons']
+    Radiant::Config['wmd.autostart'] = "false" unless Radiant::Config['wmd.autostart']
+    admin.pages.edit.add :parts_bottom, 'filter_preview', :before => 'edit_layout_and_type'
+    admin.snippets.edit.add :form_bottom, 'admin/pages/filter_preview', :after => 'edit_buttons'
   end
 
 end
